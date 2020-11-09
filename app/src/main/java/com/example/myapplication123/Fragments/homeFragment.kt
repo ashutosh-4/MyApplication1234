7354d6a0957b9071b2eb5f5b25bdb898b86e36d6
Pranav Tomar <pranavtom1223@gmail.com>
2020-11-09 17:03:08 +0530

    update

--- app/src/main/java/com/example/myapplication123/Fragments/homeFragment.kt
+++ app/src/main/java/com/example/myapplication123/Fragments/homeFragment.kt
@@ -13,26 +13,115 @@ import com.example.myapplication123.R
 
 class homeFragment : Fragment() {
 
-    private var sensorManager:SensorManager?=null
-
+    private var sensorManager: SensorManager ?= null
     private var running = false
     private var totalSteps = 0f
     private var previousTotalSteps = 0f
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_home)
+        setContentView(R.layout.activity_main)
+        loadData()
+        resetSteps()
 
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager?
 
+        fun launchIntent(view: View) {
+            val i = Intent(this, SettingsActivity::class.java)
+            startActivity(i)
+        }
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_home -> {
+                    val fragment = HomeFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.menu_report -> {
+                    val fragment = layout.ReportFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.menu_water -> {
+                    val fragment = WaterFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.menu_settings -> {
+                    val fragment = SettingsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
     }
 
+    override fun onResume() {
+        super.onResume()
+        running = true
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        if (stepSensor == null) {
+            Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
+        }else{
+            sensorManager?.registerListener(this, stepSensor,SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(running ){
+            totalSteps = event!!.values[0]
+            val currentSteps: Int = totalSteps.toInt() - previousTotalSteps.toInt()
+            tv_stepsTaken.text = ("$currentSteps")
+
+            progress_circular.apply{
+                setProgressWithAnimation(currentSteps.toFloat())
+            }
+
+        }
+    }
+    private fun resetSteps(){
+        tv_stepsTaken.setOnClickListener {
+            Toast.makeText(this, "Long tap to reset steps", Toast.LENGTH_SHORT).show()
+        }
+        tv_stepsTaken.setOnLongClickListener{
+            previousTotalSteps = totalSteps
+
+            tv_stepsTaken.text= 0.toString()
+            saveData()
+
+            true
+
+        }
+    }
+    private fun saveData(){
+        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val editor:SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putFloat("key1", previousTotalSteps)
+        editor.apply()
+    }
+
+    private fun loadData(){
+        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val savedNumber = sharedPreferences.getFloat("key1",0f)
+        Log.d("MainActivity", "$savedNumber")
+        previousTotalSteps = savedNumber
+    }
+    private fun setContent(content: String) {
+        setTitle(content)
+        tvLabel.text = content
+    }
 
 }
